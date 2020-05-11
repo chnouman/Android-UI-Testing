11b20110f8e38082e1be00b58ac8221ecdac30dd
M Nouman <muhammad.nouman@visionx.io>
2020-05-11 12:26:31 +0500

    Performed 2 tests related to Activity Navigation Scenarios

--- app/src/androidTest/java/com/mnouman/myapplication/MainActivityTest.kt
+++ app/src/androidTest/java/com/mnouman/myapplication/MainActivityTest.kt
@@ -3,6 +3,7 @@ package com.mnouman.myapplication
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -14,35 +15,33 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     /**
-     * test if activity is visible or not
+     * test if onClick second activity is opening
      * */
     @Test
-    fun test_inActivityInView() {
+    fun test_activityNavigation() {
         /*launch the activity each time*/
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        Espresso.onView(withId(R.id.main)).check(matches(isDisplayed()))
-    }
-
-    /***
-     *
-     * check if text view is displayed or not
-     * check if main Button is visible or not
-     *
-     * */
-    @Test
-    fun test_visibility_title_secondActivityBtn() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.mainTitle)).check(matches(isDisplayed()))
-        onView(withId(R.id.mainButton)).check(matches(isDisplayed()))
+        /*perform click*/
+        onView(withId(R.id.mainButton)).perform(click())
+        /*check second activity*/
+        onView(withId(R.id.mainSecond)).check(matches(isDisplayed()))
     }
 
     /**
-     *
-     * check if text view text matches the text view we are giving
+     * test if onClick second activity is opening
+     * and on back click we are coming back correctly
      * */
     @Test
-    fun test_is_textTitle_isSame() {
+    fun test_activityBackNavigation() {
+        /*launch the activity each time*/
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.mainTitle)).check(matches(withText(R.string.mainTitle)))
+        /*perform click*/
+        onView(withId(R.id.mainButton)).perform(click())
+        /*check second activity*/
+        onView(withId(R.id.mainSecond)).check(matches(isDisplayed()))
+        /*perform click*/
+        Espresso.onView(withId(R.id.secondBtn)).perform(click())
+        /*check previous activity is displayed or not*/
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 }
--- app/src/androidTest/java/com/mnouman/myapplication/SecondActivityTest.kt
+++ app/src/androidTest/java/com/mnouman/myapplication/SecondActivityTest.kt
@@ -1,53 +1,10 @@
 package com.mnouman.myapplication
 
-import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import org.junit.Assert.*
-import org.junit.Rule
-import org.junit.Test
+
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class SecondActivityTest {
 
-    @get: Rule
-    val activityRule = ActivityScenarioRule(SecondActivity::class.java)
-
-    /**
-     * test if activity is visible or not
-     * */
-    @Test
-    fun test_inActivityInView() {
-        /*launch the activity each time
-        * no need to call activitySceneario bcz we are using activity sceneario rule
-        * */
-        Espresso.onView(withId(R.id.mainSecond)).check(matches(isDisplayed()))
-    }
-
-    /***
-     *
-     * check if text view is displayed or not
-     * check if main Button is visible or not
-     *
-     * */
-    @Test
-    fun test_visibility_title_secondActivityBtn() {
-        onView(withId(R.id.secondBtn)).check(matches(isDisplayed()))
-    }
-
-    /**
-     *
-     * check if text view text matches the text view we are giving
-     * */
-    @Test
-    fun test_is_button_title_isSame() {
-        onView(withId(R.id.secondBtn)).check(matches(ViewMatchers.withText(R.string.back)))
-    }
 }
